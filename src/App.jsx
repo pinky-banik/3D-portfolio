e7844fcaa8e92f36cd4dfa26f8fc5f26816843eb
pinky-banik <pinky.rani.banik>
2023-03-28 22:50:13 +0600

    navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { BrowserRouter } from "react-router-dom"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import About from './components/About';
+import Experience from './components/Experience';
+import Tech from './components/Tech';
+import Works from "./components/Works";
+import Feedbacks from "./components/Feedbacks";
+import Contact from "./components/Contact";
+import Stars from "./components/canvas/Stars";
 
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>mew</h1>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div>
+        <Contact/>
+        <Stars/>
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../style';
+import logo from '../assets/logo.png';
+import { navLinks } from './../constants/index';
+import { close, menu } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false)
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+        to="/"
+        className='flex items-center gap-2'
+        onClick={()=>{
+          setActive("");
+          window.scrollTo(0,0);
+        }}
+        >
+          <img src={logo} alt="logo" className='w-24 object-contain'/>
+          <p className='text-white text-[18px]font-bold cursor-pointer'>Pinky Banik</p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-grow gap-10'>
+          {
+            navLinks.map((link)=>(
+              <li
+              key={link.id}
+              className={`${
+                active===link.title ? "text-active" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`
+              }
+              onClick={()=>setActive(link.title)}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))
+          }
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle ?  close : menu} alt="menu"  className='w-[28px] h-[28px] object-contain cursor-pointer'
+          onClick={()=>setToggle(!toggle)}
+          />
+
+        </div>
+      </div>
+    </nav>
   )
 }
 
