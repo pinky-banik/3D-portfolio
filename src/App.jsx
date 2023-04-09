045acce9f292688ce4552b03377a3d0764a9c237
pinky-banik <pinky.rani.banik@gmail.com>
2023-04-09 23:55:09 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,27 @@
-import { BrowserRouter } from "react-router-dom"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import About from './components/About';
-import Experience from './components/Experience';
-import Tech from './components/Tech';
-import Works from "./components/Works";
-import Feedbacks from "./components/Feedbacks";
-import Contact from "./components/Contact";
-import Stars from "./components/canvas/Stars";
+import { BrowserRouter } from "react-router-dom";
 
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
-function App() {
-
+const App = () => {
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar/>
-        <Hero/>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About/>
-      <Experience/>
-      <Tech/>
-      <Works/>
-      <Feedbacks/>
-      <div>
-        <Contact/>
-        <Stars/>
-      </div>
-    </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
